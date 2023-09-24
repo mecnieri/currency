07d5f675c84c96fd42181283613d4219b29f3fc2
abzianidze <g.abzianidze@fortunejack.com>
2023-09-25 00:35:46 +0400

    date changed

--- main.js
+++ main.js
@@ -2,17 +2,16 @@ const currencies = ['USD', 'EUR']
 const startDate = '2023-09-19T06:16:05.031Z'
 const endDate = '2023-09-21T06:16:05.031Z'
 
-const date = new Date().toISOString()
+const date = new Date(Date.now() + 4 * (60 * 60 * 1000)).toISOString()
 const niceDate = date.slice(0, 10)
 
- 
 currencies.forEach(currency => {
   fetch(
     `https://nbg.gov.ge/gw/api/ct/monetarypolicy/currencies/?currencies=${currency}&date=${date}`,
   )
     .then(response => response.json())
     .then(data => {
-      console.log(data)
+      // console.log(data)
 
       const { currencies } = data[0]
       const { rate, diff } = currencies[0]
